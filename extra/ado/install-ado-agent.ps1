af636acfbcc226c427913a11dfe636a2aafd152f
pedrogarciamf <pedro.garcia@microfocus.com>
2022-03-03 11:10:21 -0500

    fix -ArgumentList

--- extra/ado/install-ado-agent.ps1
+++ extra/ado/install-ado-agent.ps1
@@ -10,6 +10,6 @@ Invoke-WebRequest -Uri "https://vstsagentpackage.azureedge.net/agent/2.200.2/vst
 #Unzip Agent
 Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\ADO-Agent\vsts-agent-win-x64.zip", "$PWD")
 #Config Agent
-Start-Process -FilePath "cmd.exe" -ArgumentList "config.cmd", "--unattended", "--url", $adourl, "--auth", "pat", "--token", $adopat, "--pool", $adopool, "--agent", $adoagent, "--replace", "--runAsService" -Wait -WorkingDirectory "C:\ADO-Agent"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "config.cmd", "--unattended", "--url", $adourl, "--auth", "pat", "--token", $adopat, "--pool", $adopool, "--agent", $adoagent, "--replace", "--runAsService" -Wait -WorkingDirectory "C:\ADO-Agent"
 
 Remove-Item -Path "C:\ADO-Agent\vsts-agent-win-x64.zip"
